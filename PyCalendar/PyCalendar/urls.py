11e3173acff654b57bab9fd6ca1c2c3fcea4352c
Ryan <51712057+RyMaDe@users.noreply.github.com>
2022-06-30 14:55:56 +0100

    Auth (#5)
    * Simple JWT Auth Tokens added + Testing

--- PyCalendar/PyCalendar/urls.py
+++ PyCalendar/PyCalendar/urls.py
@@ -16,15 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from PyCalendar.PyCal_API import urls as api_urls
-# from rest_framework_simplejwt.views import (
-#     TokenObtainPairView,
-#     TokenRefreshView,
-# )
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(api_urls)),
-    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
