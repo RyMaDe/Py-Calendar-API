bce39456e42699f5e260a3b75c5607ddcba129f7
Ryan <r_deol@hotmail.co.uk>
2022-07-04 19:52:03 +0100

    initial schema and docs added

--- PyCalendar/PyCalendar/urls.py
+++ PyCalendar/PyCalendar/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from PyCalendar.PyCal_API import urls as api_urls
 from PyCalendar.users import urls as users_urls
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -29,4 +31,10 @@ urlpatterns = [
     path('api/user/', include(users_urls, namespace='users')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('schema', get_schema_view(
+        title="CalendarAPI",
+        description="API for the Calendar API",
+        version="1.0.0",
+    ), name="api-schema"),
+    path('docs/', include_docs_urls(title='CalendarAPI')),
 ]
