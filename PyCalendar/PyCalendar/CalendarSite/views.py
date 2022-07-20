90445bc758589a608d149105ffc3ea6464974dff
Ryan <r_deol@hotmail.co.uk>
2022-07-20 17:59:10 +0100

    Added login required mixin to main page view

--- PyCalendar/PyCalendar/CalendarSite/views.py
+++ PyCalendar/PyCalendar/CalendarSite/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views import View
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class Index(View):
+class Index(LoginRequiredMixin, View):
+    login_url = reverse_lazy("users:Login_user")
+    redirect_field_name = None
     def get(self, request):
         print(request.user)
         return render(request, "calendar.html")
