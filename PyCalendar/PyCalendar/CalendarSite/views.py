41cc544fa6bc0e66e053c2fa2fddea89ad220dbd
Ryan <r_deol@hotmail.co.uk>
2022-07-21 15:37:30 +0100

    List all user's calendar items on main page

--- PyCalendar/PyCalendar/CalendarSite/views.py
+++ PyCalendar/PyCalendar/CalendarSite/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from django.views import View
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from PyCalendar.PyCal_API.models import Calendar_API
+
 
 class Index(LoginRequiredMixin, View):
     login_url = reverse_lazy("users:Login_user")
     redirect_field_name = None
+
     def get(self, request):
         print(request.user)
-        return render(request, "calendar.html")
+        items = Calendar_API.objects.filter(Author=request.user)
+        return render(request, "calendar.html", {"items": items})
